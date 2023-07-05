56cd1f5e780858dbfc69bb3c5d4ee4d60cceeb8a
ndkar12354 <ndkarpov1@gmail.com>
2023-07-05 17:11:42 +0300

    прописали интерфейс первого окна

--- vin_1.py
+++ vin_1.py
@@ -1,6 +1,7 @@
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QVBoxLayout, QHBoxLayout
 from instr import *
+from vin_2 import *
 
 class MainWin(QWidget):
     def __init__(self):
@@ -14,10 +15,19 @@ class MainWin(QWidget):
         self.resize(win_w, win_h)
         self.move(win_x, win_y)
     def initUI(self):
-        pass
+        self.hello_text = QLabel(hello_text)
+        self.instruction = QLabel(txt_instr)
+        self.btn = QPushButton(btn_txt)
+        self.layout = QVBoxLayout()
+        self.layout.addWidget(self.hello_text)
+        self.layout.addWidget(self.instruction)
+        self.layout.addWidget(self.btn)
+        self.setLayout(self.layout)
     def connects(self):
-        pass
-
+        self.btn.clicked.connect(self.next_click)
+    def next_click(self):
+        self.hide()
+        self.tw = TestWin()
 
 app = QApplication([])
 mw = MainWin()
--- vin_2.py
+++ vin_2.py
@@ -1,2 +1,6 @@
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QVBoxLayout, QHBoxLayout
+
+class TestWin(QWidget):
+    def __init__(self):
+        pass
